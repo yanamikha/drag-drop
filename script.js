276ec596c673371acf7905b5e2815e1939c1a8a9
yanamikha <yanamikha@gmail.com>
2020-03-15 22:42:21 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+var dropTarget = document.querySelector(".wrapper");
+var draggables = document.querySelectorAll(".task");
+
+
+
+for (let i = 0; i < draggables.length; i++) {
+  draggables[i].addEventListener("dragstart", function(ev) {
+    ev.dataTransfer.setData("srcId", ev.target.id);
+  });
+}
+
+
+dropTarget.addEventListener('dragover', function(ev) {
+  ev.preventDefault();
+});
+dropTarget.addEventListener('dragleave', function(ev) {
+
+    // var title = document.getElementById("task1");
+  // task1.style.marginTop = "20px";
+
+});
+dropTarget.addEventListener('activate', function(ev) {
+  ev.preventDefault();
+});
+
+dropTarget.addEventListener('drop', function(ev) {
+  ev.preventDefault();
+  let target = ev.target;
+  let droppable = target.classList.contains('box');
+  let srcId = ev.dataTransfer.getData("srcId");
+
+  if (droppable) {
+
+    ev.target.appendChild(document.getElementById(srcId));
+  }
+  
+});
